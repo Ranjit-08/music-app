eb3d32b878f2366a5895885c277ec4fad5fb388c
Ranjit-08 <ranjitsamalarj@gmail.com>
2026-02-12 15:27:07 +0530

    correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Upload from "./pages/Upload";
@@ -6,14 +6,12 @@ import Videos from "./pages/Videos";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/videos" element={<Videos />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/videos" element={<Videos />} />
+    </Routes>
   );
 }
 
